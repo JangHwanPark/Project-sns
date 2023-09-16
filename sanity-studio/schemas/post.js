49f5efa055590f392349b4a49d9a32ddf9cbe3ad
JangHwanPark <wkd5878@gmail.com>
2023-09-16 19:28:40 +0900

    Data Modeling - Post Data

--- sanity-studio/schemas/post.js
+++ sanity-studio/schemas/post.js
@@ -1,3 +1,61 @@
+/** Post Data Model */
 export default {
-
+  title: 'Post',
+  name: 'post',
+  type: 'document',
+  fields: [
+    /** Author : 작성자 */
+    {
+      title: 'Author',
+      name: 'author',
+      type: 'reference',
+      to: [{type: 'user'}]
+    },
+    /** Photo : 게시글 이미지 */
+    {
+      title: 'Photo',
+      name: 'photo',
+      type: 'image'
+    },
+    /** Likes : 좋아요 버튼 */
+    {
+      title: 'Likes',
+      name: 'likes',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}]
+        }
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+    /** Comments : 댓글 */
+    {
+      title: 'Comments',
+      name: 'comments',
+      type: 'array',
+      of: [
+        {
+          title: 'Comment',
+          name: 'comment',
+          type: 'document',
+          fields: [
+            {
+              title: 'Author',
+              name: 'author',
+              type: 'reference',
+              to: [{type: 'user'}]
+            },
+            /** 실제 Text 스키마 */
+            {
+              title: 'Comment',
+              name: 'comment',
+              type: 'string'
+            }
+          ]
+        }
+      ]
+    }
+  ]
 }
