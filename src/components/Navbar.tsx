1af1911640cdc9296416f6ec44466a7552b310de
JangHwanPark <wkd5878@gmail.com>
2023-09-16 21:56:44 +0900

    Setting - NextAuth (Sign In, Sing Out)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,29 +9,32 @@ import NewIcon from '@/components/ui/icons/NewIcon'
 import NewFillIcon from '@/components/ui/icons/NewFillIcon'
 import {usePathname} from 'next/navigation'
 import ColorButton from '@/components/ui/ColorButton'
-import {log} from 'util'
+import {useSession, signIn, signOut} from 'next-auth/react'
+
+const menu = [
+  {
+    href: '/',
+    icon: <HomeIcon/>,
+    clickedIcon: <HomeFillIcon/>
+  },
+  {
+    href: '/search',
+    icon: <SearchIcon/>,
+    clickedIcon: <SearchFillIcon/>
+  },
+  {
+    href: '/new',
+    icon: <NewIcon/>,
+    clickedIcon: <NewFillIcon/>
+  }
+]
 
 const Navbar = () => {
-  const menu = [
-    {
-      href: '/',
-      icon: <HomeIcon/>,
-      clickedIcon: <HomeFillIcon/>
-    },
-    {
-      href: '/search',
-      icon: <SearchIcon/>,
-      clickedIcon: <SearchFillIcon/>
-    },
-    {
-      href: '/new',
-      icon: <NewIcon/>,
-      clickedIcon: <NewFillIcon/>
-    }
-  ]
   /** pathName : 현재 경로를 가져옴 */
   const pathName = usePathname();
   console.log(pathName)
+  const { data: session } = useSession();
+  console.log(session)
   return (
     <div className='flex justify-between items-center px-6'>
       <Link href='/'>
@@ -46,7 +49,11 @@ const Navbar = () => {
               </Link>
             </li>
           ))}
-          <ColorButton text='Sign In' onClick={()=>{}}/>
+          {session ? (
+              <ColorButton text='Sign In' onClick={()=> signIn()}/>
+          ) : (
+            <ColorButton text='Sign Out' onClick={()=> signOut()}/>
+          )}
         </ul>
       </nav>
     </div>
