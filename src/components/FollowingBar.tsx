7571bf1a39749650f7ce6385357871407b27a91a
JangHwanPark <wkd5878@gmail.com>
2023-09-21 11:11:01 +0900

    Feat - FollowingBar Design
    - SWR Setting (Install)
    - 백엔드에서 팔로우 데이터를 가져와 출력
    - SimpleUser, DetailUser 데이터 스키마 디자인

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,11 +1,36 @@
 'use client'
 import React from 'react'
 import useSWR from 'swr'
+import Link from 'next/link'
+import {DetailUser} from '@/model/user'
+import {FadeLoader} from 'react-spinners'
+import Avatar from '@/components/Avatar'
 
 const FollowingBar = () => {
-  const {data, error, isLoading} = useSWR('/api/hello')
-  console.log(data)
-  return <p>FollowingBar</p>
+  const {data, error, isLoading} = useSWR<DetailUser>('/api/follow')
+  const users = data?.following
+  console.log(`data?.following : ${users}`)
+  return (
+    <section>
+      { isLoading ? (
+          <FadeLoader color='red'/>
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't' have following`}</p>
+      )}
+      {users && users.length > 0 &&
+        <ul>
+          {users.map((image, username) => (
+              <li key={username}>
+                <Link href={`/user/${username}`}>
+                  <Avatar image={image} highlight/>
+                  <p>{username}</p>
+                </Link>
+              </li>
+            ))}
+        </ul>
+      }
+    </section>
+  )
 }
 
 export default FollowingBar
--- src/model/user.ts
+++ src/model/user.ts
@@ -1,4 +1,5 @@
 /**
+ * 세션에서 얻을 수 있는 사용자 정보
  * 로그인한 사용자는 name, email, username 이라는 필수 타입을 가진다.
  * 이미지는 설정 했을수도 있고 안했을수도 있기에 옵셔널로 설정
  */
@@ -7,4 +8,12 @@ export type User = {
   email: string;
   username: string;
   image?: string;
+}
+
+export type SimpleUser = Pick<User, 'username' | 'image'>;
+
+export type DetailUser = User & {
+  following: SimpleUser[];
+  followers: SimpleUser[];
+  bookmarks: string[];
 }
