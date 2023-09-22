d8b211214799e4b3ab2832a4f5cf2979fdbb9907
JangHwanPark <wkd5878@gmail.com>
2023-09-22 09:00:59 +0900

    Feat - FollowingBar ScrollableBar 구현
    - react multi carousel

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import {DetailUser} from '@/model/user'
 import {FadeLoader} from 'react-spinners'
 import Avatar from '@/components/Avatar'
+import ScrollableBar from '@/components/ui/ScrollableBar'
 
 const FollowingBar = () => {
   const {data, error, isLoading} = useSWR<DetailUser>('/api/follow')
@@ -13,32 +14,32 @@ const FollowingBar = () => {
   const users = data?.following && [
     ...data?.following,
     ...data?.following,
-    ...data?.following,
+    ...data?.following
   ]
 
   return (
-    <section className='w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
+    <section
+      className='w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
       {isLoading ? (
-          <FadeLoader color='red'/>
+        <FadeLoader color='red' />
       ) : (
         (!users || users.length === 0) && <p>{`You don't' have following`}</p>
       )}
       {users && users.length > 0 && (
-        <ul className='w-full flex gap-3'>
+        <ScrollableBar>
           {users.map(({image, username}) => (
-              <li key={username}>
-                <Link
-                  className='flex flex-col items-center w-20'
-                  href={`/user/${username}`}
-                >
-                  <Avatar image={image} highlight/>
-                  <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
-                    {username}
-                  </p>
-                </Link>
-              </li>
-            ))}
-        </ul>
+            <Link
+              key={username}
+              className='flex flex-col items-center w-20'
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
+                {username}
+              </p>
+            </Link>
+          ))}
+        </ScrollableBar>
       )}
     </section>
   )
